47b96475d5938759e1b7ddbe38aec18017c2ac77
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2021-06-06 14:00:43 +0530

    added movies.js

---
+++ src/components/Movies.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './style.css'
+
+
+export default class Movies extends Component {
+    render() {
+        return (
+            <div>
+                <header>
+                    <p className="popular">Popular Titles</p>
+                </header>
+                
+            </div>
+        )
+    }
+}
+
