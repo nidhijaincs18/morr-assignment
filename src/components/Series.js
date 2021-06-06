d264124b740b626759d9b8bd18818253732d369c
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2021-06-06 14:01:05 +0530

    added series.js

---
+++ src/components/Series.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react'
+import './style.css'
+
+
+export default class Series extends Component {
+    render() {
+        return (
+            <div>
+                <header>
+                    <p className="popular">Popular Titles</p>
+                </header>
+                
+            </div>
+        )
+    }
+}
